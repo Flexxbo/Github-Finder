cf6202a6fb6d2937060e318d77532c51542b5333
Flexxbo <flexxbo@gmail.com>
2020-06-29 16:46:08 +0200

    added clear button

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Search extends Component {
   state = { text: "" };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+  };
+
   onSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: "" });
   };
   onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
@@ -27,6 +36,14 @@ export class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
+        {this.props.showClear && (
+          <button
+            className='btn btn-light btn-block'
+            onClick={this.props.clearUsers}
+          >
+            Clear
+          </button>
+        )}
       </div>
     );
   }
