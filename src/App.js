f7cf1c732a20e1879efae995516ec7f39ec8ad89
Flexxbo <flexxbo@gmail.com>
2020-06-29 15:32:42 +0200

    added searchbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className='App'>
         <Navbar title={"Github Finder"} icon='fab fa-github' />
-        <Users />
+        <div className='container'>
+          <Search />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
