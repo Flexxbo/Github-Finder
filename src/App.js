bc7e8bdeda73f12f050b467f0c2f0f92e8358bf1
Flexxbo <flexxbo@gmail.com>
2020-06-30 22:51:05 +0200

    changed display

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
   //setAlert if input field is empty
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 5000);
   };
 
   render() {
