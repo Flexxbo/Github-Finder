4055f7d6a721c4b7ddb676cc99106effe731e873
Flexxbo <flexxbo@gmail.com>
2020-07-06 14:25:39 +0200

    added Alert-Context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,14 +1,13 @@
 import React, { useEffect, Fragment, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import GithubContext from "../../context/github/githubContext";
 
-const User = ({ getUserRepos, repos, match }) => {
-const githubContext = useContext(GithubContext)
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
 
-  const { getUser, loading, user } = githubContext;
+  const { getUser, loading, user, repos, getUserRepos } = githubContext;
 
   useEffect(() => {
     getUser(match.params.login);
@@ -96,9 +95,4 @@ const githubContext = useContext(GithubContext)
   );
 };
 
-User.propTypes = {
- 
-  repos: PropTypes.array.isRequired,
-  getUserRepos: PropTypes.func.isRequired,
-};
 export default User;
