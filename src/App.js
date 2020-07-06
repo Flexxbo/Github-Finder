9fdf301f150541d65cc31e14657adada7e8f27f5
Flexxbo <flexxbo@gmail.com>
2020-07-06 13:06:21 +0200

    moved searchUsers to AppLevelState with Context

--- src/App.js
+++ src/App.js
@@ -18,17 +18,6 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // +++ Search Github users +++
-
-  const searchUsers = async (text) => {
-    setLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
   //Get Single Github User
   const getUser = async (username) => {
     setLoading(true);
@@ -76,7 +65,6 @@ const App = () => {
                 render={(props) => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length > 0 ? true : false}
                       setAlert={showAlert}
